086ba67e9554c74b19862bc1c218e6912cede5af
Muhammad Ihsan <muhihsan0@outlook.com>
2024-11-27 23:09:07 +0700

    changed kafka docker service

--- run_program.sh
+++ run_program.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 bin/kafka-topics.sh --create --topic  test_data --bootstrap-server localhost:9092
 
-python python_script/streaming_simulation.py
-python python_script/pyspark_consumer.py
+# python python_script/streaming_simulation.py
+# python python_script/pyspark_consumer.py
