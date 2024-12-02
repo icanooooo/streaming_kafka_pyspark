952516a2c3fd70b765906d660fdaeeff60d00b16
Muhammad Ihsan <muhihsan0@outlook.com>
2024-12-02 12:46:11 +0700

    resolved offset issue, currently jdbc driver issue

--- python_script/pyspark_consumer.py
+++ python_script/pyspark_consumer.py
@@ -1,18 +1,50 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StringType, StructType, StructField, TimestampType
 from pyspark.sql.functions import from_json, col
+from helper.postgres_helper import create_connection, load_query
 
-def ensureTable(conn):
-    print("yes")
+def ensureTable():
+    conn = create_connection('localhost', 5432, 'destination_db', 'icanooo', 'rahasia')
+
+    query = """
+    CREATE TABLE IF NOT EXISTS test_table (
+        id VARCHAR(50),
+        name VARCHAR(50),
+        age varchar(50),
+        submitted_time TIMESTAMP 
+    );
+    """
+
+    load_query(conn, query)
+
+    conn.commit()
+    conn.close()
+
+def to_postgres(batch_df, batch_id):
+    try:
+        batch_df.write \
+            .format("jdbc") \
+            .option("url", "jdbc:postgresql://localhost:5432/destination_db") \
+            .option("dbtable", "test_table") \
+            .option("user", "icanooo") \
+            .option("password", "rahasia") \
+            .mode("append") \
+            .save()
+    except Exception as e:
+        print(f"error: {e}")
 
 
 if __name__ == "__main__":
+    ensureTable()
+    
     # Create SparkSession
     spark = SparkSession.builder \
         .appName("Test-PySpark") \
         .master("local[*]") \
         .config("spark.jars.packages",
                 "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
+        .config("spark.jars",
+                "/home/icanooo/Desktop/DE/streaming_project/postgresql-42.7.4.jar") \
         .getOrCreate()
 
     # kafka configuration
@@ -24,6 +56,7 @@ if __name__ == "__main__":
         .format("kafka") \
         .option("kafka.bootstrap.servers", kafka_broker) \
         .option("subscribe", kafka_topic) \
+        .option("startingOffsets", "earliest") \
         .load()
 
     # Defining Schema
@@ -40,9 +73,8 @@ if __name__ == "__main__":
 
     # Display data
     query = parsed_stream.writeStream \
+        .foreachBatch(to_postgres) \
         .outputMode("append") \
-        .format("console") \
-        .option("checkpointLocation", "/home/icanooo/Desktop/DE/streaming_project/checkpoint") \
         .start()
 
     query.awaitTermination()
